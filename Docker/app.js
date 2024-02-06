cd5c7e78a7b8dc1f3e491c81949a08eafb445823
supachaiaware <supachai.y@aware.co.th>
2024-02-06 08:16:02 +0000

    Update

--- Docker/app.js
+++ Docker/app.js
@@ -1,11 +1,16 @@
-const express = require('express')
-const app = express()
-const port = 80
+const express = require('express');
+const app = express();
+const port = 80;
 
 app.get('/', (req, res) => {
-  res.send('Hello Supachai')
-})
+  res.send('Hello Supachai!');
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Server is running on http://localhost:${port}`);
+});
+
+// Handling errors
+app.on('error', (err) => {
+  console.error(`Server error: ${err.message}`);
+});
